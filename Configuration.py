d9e1bdf7b255ea5840c4883e9532f99cb1cbe091
Soumya Suvra Ghosal <52136583+Soumya1612-Rasha@users.noreply.github.com>
2019-07-21 03:04:07 -0400

    Add files via upload

---
+++ Configuration.py
@@ -0,0 +1,16 @@
+
+class Cfg:
+    image_height = 28
+    image_width = 28
+    epochs=25
+    batch_size=16
+    channels=1
+    batch_size = 32
+    num_classes=10
+    dynamic=0
+    size=0.3
+    plotHistory=1
+    random_state=2007
+    evals=100
+    learn_rate=0.0002
+    momentum=0.5
